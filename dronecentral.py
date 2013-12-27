718e166301951e5555172f2420ec0c9cdfd3120c
David Segersson <david.segersson@smhi.se>
2013-12-27 12:45:11 +0100

    Version 0.1

--- dronecentral.py
+++ dronecentral.py
@@ -1,16 +1,18 @@
 import os
-import abc
-import cgi
 import webapp2
 import jinja2
+import logging
 from google.appengine.api import urlfetch
 from google.appengine.ext import db
 
 DEFAULT_SURVEY_NAME = "ACTIVESURVEY"
-
 GCM_SERVER = "https://android.googleapis.com/gcm/send"
 GOOGLE_API_KEY = "AIzaSyBes8BF3izsF6xmMAkqFl5LOFKXOcYcswU"
-REGISTRATION_ID = "Registration Id of the target device"
+
+MIN_RUDDER_ANGLE = -90
+MAX_RUDDER_ANGLE = 90
+MIN_LOAD = 0
+MAX_LOAD = 100
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -18,12 +20,25 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 
+def num_in_range(val, min_val, max_val):
+    try:
+        load = float(val)
+        if load >= min_val and load <= max_val:
+            return True
+    except:
+        return False
+
+
 def survey_key(survey_name=DEFAULT_SURVEY_NAME):
     """
     Constructs a Datastore key for a Survey entity
     with survey_name.
     """
-    return db.Key(survey_name)
+    return db.Key.from_path('WaypointList', survey_name)
+
+
+class Device(db.Model):
+    device_id = db.StringProperty(indexed=False)
 
 
 class StatusReport(db.Model):
@@ -45,51 +60,175 @@ class WaypointList(db.Model):
     waypoints = db.ListProperty(db.GeoPt)
 
 
-class BaseMessage(webapp2.RequestHandler):
+class SendMessage(webapp2.RequestHandler):
     """
     Class containing basic functionality to communicate
     with drone using GCM
     """
 
-    def makeRequest(self):
-        """Create request to GCM"""
-        request = "registration_ids=%i&command=%s&value=%s" % (
-            REGISTRATION_ID,
-            self.getCommand(),
-            self.getValue())
-        return request
-
-    def makeNotification(self, msg, result):
+    def makeNotification(self, cmd, val, result):
         """ Show notification of sent message and received feedback"""
         self.response.headers['Content-Type'] = 'text/html'
         self.response.set_status(200, "OK" )
-        self.response.out.write('<html>')
-        self.response.out.write('<head>')
-        self.response.out.write('<title>')
-        self.response.out.write('Notifiation')
-        self.response.out.write('</title>')
-        self.response.out.write('</head>')
-        self.response.out.write('<body>')
-        self.response.out.write('Sent message: %s' % msg)
-        self.response.out.write('Server response, status: %s' % result.content)
-        self.response.out.write('</body>')
-        self.response.out.write('</html>')
 
-    @abc.abstractmethod
-    def getValue(self):
-        """Abstract method to acquire value for message"""
+        template_values = {
+            'cmd': cmd,
+            'val': val,
+            'content': result.content
+        }
 
-    @abc.abstractmethod
-    def getCommand(self):
-        """Abstract method to acquire command for message"""
+        template = JINJA_ENVIRONMENT.get_template('message.html')
+        self.response.write(template.render(template_values))
 
-    def post(self):
+
+def get_device_id():
+    dev_query = Device.all()
+    devices = dev_query.fetch(1)
+    return devices[0].device_id
+
+
+def getReports():
+    report_query = StatusReport.all()
+    reports = report_query.ancestor(
+        survey_key(DEFAULT_SURVEY_NAME)).fetch(10)  # get 10 last reports
+
+    logging.info("Found %i reports in datastore" % len(reports))
+    return reports
+
+
+def getWaypoints():
+    """Get waypoints from datastore and return as javascript"""
+    query = WaypointList.all()
+    wp_list = query.ancestor(survey_key(DEFAULT_SURVEY_NAME)).fetch(1)
+    if wp_list != []:
+        logging.info("Getting %i waypoints" % len(wp_list[0].waypoints))
+        return wp_list[0].waypoints
+    else:
+        logging.info("No waypoints found in datastore")
+        return []
+
+        return []
+
+
+class RegisterDevice(webapp2.RequestHandler):
+
+    def post(self, device_id):
+        dev = Device(device_id=device_id)
+        dev.put()
+        self.response.write("OK")
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def render(self, command="", response="", alert=""):
+        """Write main page"""
+        template_values = {
+            'google_api_key': GOOGLE_API_KEY,
+            'waypoints': getWaypoints(),
+            'reports': getReports(),
+            'command': command,
+            'response': response,
+            'min_rudder_angle': MIN_RUDDER_ANGLE,
+            'max_rudder_angle': MAX_RUDDER_ANGLE,
+            'min_load': MIN_LOAD,
+            'max_load': MAX_LOAD,
+            'alert': alert,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        return template.render(template_values)
+
+    def delete_reports(self):
+        db.delete(StatusReport.all())
+        self.response.write(
+            self.render(
+                alert='<script> alert("Deleted reports from db");</script>'))
+
+    def delete_waypoints(self):
+        db.delete(WaypointList.all())
+        self.response.write(
+            self.render(
+                alert='<script> alert("Deleted survey from db");</script>'))
+
+    def upload_file(self):
+        self.delete_waypoints()
+        f = self.request.POST.get('file').file
+        waypoint_string = f.read()
+        try:
+            waypoints = [map(float, wp.split(',')) for wp in
+                         waypoint_string.split("\n")[:-1]]
+        except:
+            self.response.out.write(
+                'Invalid file, should be , separated' +
+                'coordinates and no header: %s' % str(waypoints))
+            return
+        waypoint_list = WaypointList(waypoints=[db.GeoPt(wp[1], wp[0])
+                                   for wp in waypoints],
+                                     parent=survey_key(DEFAULT_SURVEY_NAME))
+        waypoint_list.put()
+        logging.info("Stored waypoints in data store")
+        self.response.write(
+            self.render(
+                    alert='<script> alert("Saved file to db");</script>'))
+
+    def upload_waypoints(self):
+        self.delete_waypoints()
+        waypoint_string = self.request.get("waypoint-list")
+        waypoints = [map(float, wp.split(',')) for wp in
+                     waypoint_string.split()]
+        waypoint_list = WaypointList(waypoints=[db.GeoPt(wp[1], wp[0])
+                                                for wp in waypoints],
+                                     parent=survey_key(DEFAULT_SURVEY_NAME))
+
+        logging.info("Created wp-list has %i nodes" % len(
+                waypoint_list.waypoints))
+
+        waypoint_list.put()
+        logging.info("Stored waypoints in data store")
+        self.response.write(
+            self.render(
+                    alert='<script> alert("Saved survey wp to db");</script>'))
+
+    def upload_report(self):
+        survey_name = self.request.get('survey_name',
+                                       DEFAULT_SURVEY_NAME)
+        report = StatusReport(
+            autopilot=self.request.get('autopilot', 'true').lower() == '1',
+            active=self.request.get('active', 'true').lower() == '1',
+            current_waypoint=int(float(self.request.get('cwp', -1))),
+            speed=float(self.request.get('speed', -1)),
+            heading=float(self.request.get('heading', -1)),
+            turn_rate=float(self.request.get('turn_rate', -1)),
+            lon=float(self.request.get('lon', -1)),
+            lat=float(self.request.get('lat', -1)),
+            battery_status=int(float(self.request.get('battery_status', -1))),
+            parent=survey_key(survey_name))
+
+        report.put()
+        logging.info("Saved report to db")
+        self.response.write(
+            self.render(
+                alert='<script> alert("Saved report wp to db");</script>'))
+
+    def send_message(self, cmd, val, collapse_key=None):
         """ Send message using GCM and wait for feedback"""
 
-        request = self.makeRequest()
+        if collapse_key is not None:
+            request = (
+                "collapse_key=%s&registration_id=%i&command=%s&value=%s" % (
+                    collapse_key,
+                    get_device_id(),
+                    cmd,
+                    val))
+        else:
+            request = "registration_id=%i&command=%s&value=%s" % (
+                get_device_id(),
+                cmd,
+                val)
 
-        result = urlfetch.fetch(
-            url="GCM_SERVER",
+        logging.info("Sending GET-STATUS")
+        response = urlfetch.fetch(
+            url=GCM_SERVER,
             payload=request,
             method=urlfetch.POST,
             headers={'Content-Type':
@@ -97,127 +236,102 @@ class BaseMessage(webapp2.RequestHandler):
                      'Authorization': 'key=' + GOOGLE_API_KEY}
             )
 
-        self.makeNotification(result)
+        logging.info("Status is %i" % response.status_code)
+        logging.info("Response is %s" % response.content)
+        if response.status_code == 200:
+            self.render()
+#            self.render(command=cmd, response=result.content,
+#                        alert='<script> alert("Sent command ' +
+#                        '%s, with value %s");</script>' % (cmd, val))
+        else:
+            self.render()
 
-
-class GetStatus(BaseMessage):
-
-    def getCommand(self):
-        return "GETSTATUS"
-
-    def getValue(self):
-        return ""
-
-
-class AddWaypoint(BaseMessage):
-
-    def getCommand(self):
-        return "SETWP"    
-
-    def getValue(self):
-        lat = cgi.escape(self.request.get('wp_lat'))
-        lon = cgi.escape(self.request.get('wp_lon'))
-        return "%s,%s" % (lon, lat)
-
-
-class DeleteWaypoints(webapp2.RequestHandler):
-    """Delete all waypoint lists from datastore"""
-
-    def post():
-        wp_list = WaypointList.get(DEFAULT_SURVEY_NAME)
-        wp_list.delete()
-
-
-class DeleteReports(webapp2.RequestHandler):
-    """Delete all reports from datastore"""
-
-    def post():
-        query = StatusReport.all()
-        reports = query.fetch(200)
-
-        for report in reports:
-            report.delete()
-
-
-class PlotWaypoints(webapp2.RequestHandler):
-    """Plot waypoints on map"""
-
-
-class PlotReports(webapp2.RequestHandler):
-    """Plot reports on map"""
-
-
-class UploadWaypoints():
-    """Upload waypoint list to datastore"""
-
-    def post(self):
-        survey_name = self.request.get('survey_name',
-                                       DEFAULT_SURVEY_NAME)
-        waypoint_list = WaypointList(parent=survey_key(survey_name))
-        waypoint_string = self.request.POST.get('waypoint-file').file.read()
-        waypoints = (map(float, wp.split(',')) for
-                     wp in waypoint_string.split(";"))
-        waypoint_list.waypoints = [db.GeoPt(wp[1], wp[0]) for wp in waypoints]
-        waypoint_list.put()
-
-
-class SetStatus(webapp2.RequestHandler):
-    """Upload status report to datastore"""
-
-    def post(self):
-        survey_name = self.request.get('survey_name',
-                                       DEFAULT_SURVEY_NAME)
-        report = StatusReport(parent=survey_key(survey_name))
-        report.autopilot = self.request.get('autopilot').lower() == 'true'
-        report.active = self.request.get('active').lower() == 'true'
-        report.current_waypoint = int(float(self.request.get('cwp')))
-        report.speed = float(self.request.get('speed'))
-        report.heading = float(self.request.get('heading'))
-        report.turn_rate = float(self.request.get('turn_rate'))
-        report.battery_status = int(float(self.request.get('battery_status')))
-        report.put()
-        #query_params = {'survey_name': survey_name}
-        #self.redirect('/?' + urllib.urlencode(query_params))
-
-
-def getWaypoints():
-    """Get waypoints from datastore and return as javascript"""
-    try:
-        wp_list = db.get(DEFAULT_SURVEY_NAME)
-    except:
-        return [db.GeoPt(58.2, 15.1),
-                db.GeoPt(58.22, 15.101)]
-
-    return wp_list
-
-
-class MainPage(webapp2.RequestHandler):
+    def get_survey(self):
+        query = WaypointList.all()
+        wp_list = query.ancestor(survey_key(DEFAULT_SURVEY_NAME)).fetch(1)
+        out = ""
+        if wp_list == []:
+            return ""
+        wp = wp_list.waypoints
+        out += "%f,%f" % (wp[0].lng(), wp[0].lat())
+        for i in range(1, len(wp)):
+            out += " %f,%f" % (wp[0].lng(), wp[0].lat())
+        return out
 
     def get(self):
-        """Write main page"""
-        report_query = StatusReport.all()
-        reports = report_query.fetch(10)
+        self.response.write(self.render())
 
-        for report in reports:
-            # Add to map
-            pass
+    def post(self):
+        logging.info("entering main.post")
+        do = self.request.get('do')
 
-        template_values = {
-            'google_api_key': GOOGLE_API_KEY,
-            'waypoints': getWaypoints()
-        }
+        if do == 'delete-waypoints':
+            logging.info("do=delete-waypoints")
+            self.delete_waypoints()
 
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
+        elif do == 'upload-file':
+            logging.info("do=upload-file")
+            self.upload_file()
+
+        elif do == 'upload-waypoints':
+            logging.info("do=upload-waypoints")
+            self.upload_waypoints()
+
+        elif do == 'upload-report':
+            logging.info("do=upload-report")
+            self.upload_report()
+
+        elif do == 'delete-reports':
+            logging.info("do=delete-reports")
+            self.delete_reports()
+
+        elif do == 'send-waypoints':
+            logging.info('do==send-waypoints')
+            self.send_message('ADD-WP', self.request.get('value', ""))
+
+        elif do == 'clear-waypoints':
+            logging.info('do==clear-waypoints')
+            self.send_message('CLEAR-WP', "")
+
+        elif do == 'send-survey':
+            logging.info('do==send-survey')
+            survey = self.get_survey()
+            if survey != []:
+                self.send_message('ADD-WP', )
+
+        elif do == 'clear-survey':
+            logging.info('do==clear-survey')
+            self.send_message('CLEAR-SURVEY', "")
+
+        elif do == 'get-status':
+            logging.info('do==get-status')
+            self.send_message('GET-STATUS', '', 'GET-STATUS')
+
+        elif do == 'set-load':
+            logging.info('do==set-load')
+            load = self.request.get('value', None)
+            if num_in_range(load, MIN_LOAD, MAX_LOAD):
+                self.send_message('SET-LOAD', load)
+
+        elif do == 'set-rudder-angle':
+            logging.info('do==set-rudder-angle')
+            angle = self.request.get('value', None)
+            if num_in_range(angle, MIN_RUDDER_ANGLE, MAX_RUDDER_ANGLE):
+                self.send_message('SET-RUDDER', angle)
+
 
 application = webapp2.WSGIApplication(
     [
         ('/', MainPage),
-        ('/get-status', GetStatus),
-        ('/report', SetStatus),
-        ('/upload', UploadWaypoints),
-        ('/delete-waypoints', DeleteWaypoints),
-        ('/add-waypoints', AddWaypoint),
-        ('/plot-report', PlotReports),
-        ('/delete-reports', DeleteReports)
+        ('/send-message', SendMessage),
+        ('/register-device', RegisterDevice),
         ], debug=True)
+
+
+def main():
+    logging.getLogger().setLevel(logging.DEBUG)
+    webapp2.util.run_wsgi_app(application)
+
+
+if __name__ == "__main__":
+    main()
